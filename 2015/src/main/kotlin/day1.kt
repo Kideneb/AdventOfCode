bc1d88c88ee2df4b111b15cd3b38876682b1aca1
Benedikt Hahn <bhahnskul@gmail.com>
2021-12-01 13:32:08 +0100

    Solve 2015.1

--- 2015/src/main/kotlin/day1.kt
+++ 2015/src/main/kotlin/day1.kt
@@ -1,7 +1,24 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    var content = File("C:\\Users\\Benedikt Hahn\\Desktop\\Ordnerstruktur\\Code\\Git\\Github\\AdventOfCode\\2015\\src\\main\\resources\\day1.txt").readLines()[0]
+    var x = 0
+    var n = 0
+    content.forEach { c -> n++
+                            if (c.equals('(')) {
+                                x++
+                            } else {
+                                x--
+                            }
+                            if (x == -1) {
+                                println(n)
+                            }
+                        }
+    println(x)
 }
